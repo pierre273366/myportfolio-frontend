3d266c50d6de5dd872dbe8108fb5dbd674bfc342
Pierre Roitg <pierre.roitg@live.fr>
2024-12-05 14:09:21 +0100

    mail

--- script-contact.js
+++ script-contact.js
@@ -2,25 +2,35 @@ function goToHome() {
   window.location.href = "index.html";
 }
 
-/*formulaire pour envoie de mail :
+/*formulaire pour envoie de mail :*/
 
-const formul = document.getElementsByClassName("form");
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".btn-2").addEventListener("click", function (event) {
+    event.preventDefault();
 
-const formEvent = formul.addEventListener("btn-2", (event) => {
-  event.preventDefault();
+    const name = document.querySelector('input[name="name"]').value;
+    const societe = document.querySelector('input[name="societe"]').value;
+    const phone = document.querySelector('input[name="phone"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const message = document.querySelector('textarea[name="message"]').value;
 
-  let mail = new FormData(formul);
-
-  sendMail(mail);
-});
-
-const sendMail = (mail) => {
-  
-  fetch("/send", {
-    method: "post", 
-    body: mail, 
-
-  }).then((response) => {
-    return response.json();
+    fetch("http://localhost:6000//mail/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, societe, phone, email, message }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "Email sent successfully!") {
+          alert("Email sent successfully!");
+        } else {
+          alert("Failed to send email. Error: " + data.error);
+        }
+      })
+      .catch((error) => {
+        alert("Failed to send email. Error: " + error.toString());
+      });
   });
-};*/
+});
