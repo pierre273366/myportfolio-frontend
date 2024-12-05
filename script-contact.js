66a29a9c0cc433fc1e18816369a6291f46220273
Pierre Roitg <pierre.roitg@live.fr>
2024-12-05 15:49:22 +0100

    maj

--- script-contact.js
+++ script-contact.js
@@ -4,46 +4,30 @@ function goToHome() {
 
 /*formulaire pour envoie de mail :*/
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".btn-2").addEventListener("click", function (event) {
-    event.preventDefault();
-
-    const name = document.querySelector('input[name="name"]').value;
-    const societe = document.querySelector('input[name="societe"]').value;
-    const phone = document.querySelector('input[name="phone"]').value;
-    const email = document.querySelector('input[name="email"]').value;
-    const message = document.querySelector('textarea[name="message"]').value;
-
-    fetch("https://myportfolio-backend-sigma.vercel.app/mail/send-email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        societe: societe,
-        phone: phone,
-        email: email,
-        message: message,
-      }),
+document.querySelector(".btn-2").addEventListener("click", (e) => {
+  e.preventDefault(); // Empêche le formulaire de se soumettre normalement
+  const formData = {
+    name: document.querySelector('[name="name"]').value,
+    societe: document.querySelector('[name="societe"]').value,
+    phone: document.querySelector('[name="phone"]').value,
+    email: document.querySelector('[name="email"]').value,
+    message: document.querySelector('[name="message"]').value,
+  };
+  fetch("https://myportfolio-backend-sigma.vercel.app/api/mail", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
     })
-      .then((response) => {
-        if (!response.ok) {
-          return response.json().then((err) => {
-            throw new Error(err.error);
-          });
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data.success) {
-          alert("Email sent successfully!");
-        } else {
-          alert("Failed to send email. Error: " + data.error);
-        }
-      })
-      .catch((error) => {
-        alert("Failed to send email. Error: " + error.toString());
-      });
-  });
+    .catch((error) => {
+      console.error(error);
+      alert(
+        "Une erreur s'est produite lors de l'envoi de l'e-mail. Veuillez réessayer."
+      );
+    });
 });
