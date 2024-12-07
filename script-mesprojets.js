f23bc231ba518fefa3b8cbd5cc2c6db9d9d87644
Pierre Roitg <pierre.roitg@live.fr>
2024-12-07 20:52:58 +0100

    media smartphone

--- script-mesprojets.js
+++ script-mesprojets.js
@@ -1,29 +1,23 @@
 //Carrouselle:
 
-// Initialisation de Swiper
 const swiper = new Swiper(".swiper", {
-  // Paramètres de base pour Swiper
-  slidesPerView: 1, // Affiche 1 slide à la fois
-  spaceBetween: 20, // Espacement entre les slides
-  loop: true, // Permet la boucle infinie
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
   autoplay: {
-    delay: 2500, // Temps avant de passer à un autre slide (en ms)
-    disableOnInteraction: false, // L'autoplay continue après l'interaction
+    delay: 2500,
+    disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination", // Éléments de pagination (les points)
-    clickable: true, // Rendre les points cliquables
-  },
-  navigation: {
-    nextEl: ".swiper-button-next", // Flèche "suivant"
-    prevEl: ".swiper-button-prev", // Flèche "précédent"
+    el: ".swiper-pagination",
+    clickable: true,
   },
   breakpoints: {
     768: {
-      slidesPerView: 2, // Sur les écrans plus larges, afficher 2 slides
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3, // Sur les écrans encore plus larges, afficher 3 slides
+      slidesPerView: 3,
     },
   },
 });
--- script-presentation.js
+++ script-presentation.js
@@ -5,18 +5,15 @@ function goToHome() {
 document.addEventListener("DOMContentLoaded", () => {
   const textElement = document.querySelector(".whoiam");
 
-  // Déclencher l'animation de fade-in, zoom-in et slide-up après un délai
   setTimeout(() => {
     textElement.classList.add("visible");
-  }, 500); // 500ms pour laisser le temps de charger la page
+  }, 500);
 
-  // Animation pour le bouton "Retour"
   const returnButton = document.querySelector(".button-retour");
   returnButton.addEventListener("click", () => {
-    // On pourrait ajouter un effet visuel avant de revenir à l'accueil
     returnButton.innerHTML = "Retour en cours...";
     setTimeout(() => {
       window.location.href = "../pages/index.html";
-    }, 500); // Délai de 500ms pour l'effet avant de rediriger
+    }, 500);
   });
 });
